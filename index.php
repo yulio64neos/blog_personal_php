ff70a095a43adbca3fbced5e88830ac1837434fa
yulio64neos <julio64neos@gmail.com>
2024-01-02 00:19:35 -0600

    Blog de entrada 1

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <!--Cabecera-->
+        <header id="header">
+            <!--LOGO-->
+            <div id="logo">
+                <a href="index.php">
+                    Blog de videojuegos
+                </a>
+            </div>
+            <!--Menu-->
+            <nav id="nav">
+                <ul>
+                    <li>
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 3</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 4</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Sobre mi</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+    <!--Barra Lateral-->
+
+    <!--Caja Principal-->
+
+    <!--Pié de página-->
+</body>
+</html>
